0def9c1ee66d28f9ef0b15cbd2719d893ff997a2
EliasFernandes <eliasneto0012@gmail.com>
2025-09-23 15:20:49 -0300

    Initial setup

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/teste', function () {
+    return response()->json([
+        'message' => 'API is running',
+        'status' => 'success'
+    ]);
+});
+
+
